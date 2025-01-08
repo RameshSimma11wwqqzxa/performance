ea60e79f421f54518fa8e9c8ef1c69004fbde8c7
Ramesh Simma <ramesh.kumar.simma@accenture.com>
2025-01-08 11:19:51 +0530

    code changes

---
+++ hybris/bin/custom/training/trainingtest/gensrc/org/training/test/constants/GeneratedTrainingTestConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 7, 2025, 12:38:41 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.training.test.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingTestConstants
+{
+	public static final String EXTENSIONNAME = "trainingtest";
+	
+	protected GeneratedTrainingTestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
